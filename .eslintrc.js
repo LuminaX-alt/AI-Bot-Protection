e394559fa67809fd50ebf371b5dc7854bf7dae64
Dev Sharma <12devsharma10c@gmail.com>
2025-07-19 23:53:31 +0530

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  root: true,
+  // This tells ESLint to load the config from the package `eslint-config-vercel-ai`
+  extends: ['vercel-ai'],
+  settings: {
+    next: {
+      rootDir: ['apps/*/'],
+    },
+  },
+};
