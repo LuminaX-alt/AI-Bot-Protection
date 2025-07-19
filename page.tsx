48c6a3ae89c7f5bef7f009152f7010ba059ef81e
Dev Sharma <12devsharma10c@gmail.com>
2025-07-20 00:13:14 +0530

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,7 @@
+
+import { generateId } from 'ai';
+import Chat from './chat/[chatId]/chat';
+
+export default async function ChatPage() {
+  return <Chat chatData={{ id: generateId(), messages: [] }} isNewChat />;
+}
